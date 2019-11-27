d68d38c8adbc5490656703ba3270b002995fb951
Melvin <melvin.vmegen@gmail.com>
2019-11-27 22:16:11 +0100

    typingjs

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -12,61 +12,106 @@ function scrollFunction() {
 };
 // End sticky bar
 
-// Begin moving letters
-$(document).ready(function () {
-var ml4 = {};
-ml4.opacityIn = [0,1];
-ml4.scaleIn = [0.2, 1];
-ml4.scaleOut = 3;
-ml4.durationIn = 800;
-ml4.durationOut = 600;
-ml4.delay = 500;
+// typed.js
+const sleep = delay => new Promise(resolve => setTimeout(resolve, delay))
+const humanize = speed => Math.round(Math.random() * (100 - 30)) + speed
+async function typed(opts) {
+  let {
+    strings,
+    elm,
+    backDelay = 500,
+    eraseSpeed = 10,
+    typeSpeed = 30,
+    loop = true
+  } = opts
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml4 .letters-1',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-1',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-2',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-2',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-3',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-3',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4',
-    opacity: 0,
-    duration: 500,
-    delay: 500
-  });
-});
-// End moving letters
+  do {
+    let last = strings.length
+    let index = 0
+
+    for (let string of strings) {
+      index++
+
+      for (let character of string) {
+        await sleep(humanize(typeSpeed))
+        elm.innerText += character
+      }
+
+      if (!loop && last == index) return
+
+      await sleep(backDelay)
+
+      while (string) {
+        await sleep(humanize(eraseSpeed))
+        elm.innerText = string = string.slice(0, -1)
+      }
+    }
+  } while (loop)
+}
+
+typed({
+  elm: document.querySelector(".typing"),
+  strings: ['du marketing digital.', 'du développement web.', 'de la traduction.'],
+  // loop: false <-- default
+}).then(() => console.log('done with animation'))
+
+// end typed.js
+
+// // Begin moving letters
+// $(document).ready(function () {
+// var ml4 = {};
+// ml4.opacityIn = [0,1];
+// ml4.scaleIn = [0.2, 1];
+// ml4.scaleOut = 3;
+// ml4.durationIn = 800;
+// ml4.durationOut = 600;
+// ml4.delay = 500;
+
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml4 .letters-1',
+//     opacity: ml4.opacityIn,
+//     scale: ml4.scaleIn,
+//     duration: ml4.durationIn
+//   }).add({
+//     targets: '.ml4 .letters-1',
+//     opacity: 0,
+//     scale: ml4.scaleOut,
+//     duration: ml4.durationOut,
+//     easing: "easeInExpo",
+//     delay: ml4.delay
+//   }).add({
+//     targets: '.ml4 .letters-2',
+//     opacity: ml4.opacityIn,
+//     scale: ml4.scaleIn,
+//     duration: ml4.durationIn
+//   }).add({
+//     targets: '.ml4 .letters-2',
+//     opacity: 0,
+//     scale: ml4.scaleOut,
+//     duration: ml4.durationOut,
+//     easing: "easeInExpo",
+//     delay: ml4.delay
+//   }).add({
+//     targets: '.ml4 .letters-3',
+//     opacity: ml4.opacityIn,
+//     scale: ml4.scaleIn,
+//     duration: ml4.durationIn
+//   }).add({
+//     targets: '.ml4 .letters-3',
+//     opacity: 0,
+//     scale: ml4.scaleOut,
+//     duration: ml4.durationOut,
+//     easing: "easeInExpo",
+//     delay: ml4.delay
+//   }).add({
+//     targets: '.ml4',
+//     opacity: 0,
+//     duration: 500,
+//     delay: 500
+//   });
+// });
+// // End moving letters
 
 // Begin Gallery
 var slideIndex = 1;
@@ -84,15 +129,15 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("myslides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex -1].style.display = "block";  
+  slides[slideIndex -1].style.display = "block";
   dots[slideIndex -1].className += " active";
 };
 // End Gallery
@@ -136,4 +181,4 @@ function isElementInViewport(el) {
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
-// End Scroll Animations      
+// End Scroll Animations
