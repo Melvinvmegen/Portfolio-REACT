7a819b3c51e4f6545ca1583447c566fdb8dd673d
Melvin <melvin.vmegen@gmail.com>
2020-02-08 12:56:59 +0100

    Updates

---
+++ precache-manifest.f0202169f3287fbd1adf5fece0c1d39c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4465309c7ef0d584de4365acaabd61bf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "52c8b61f1c95b56a94e6",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "86b0b4d7b0984a4cb1ad",
+    "url": "/portfolio/static/css/main.ccd6fd4d.chunk.css"
+  },
+  {
+    "revision": "52c8b61f1c95b56a94e6",
+    "url": "/portfolio/static/js/2.0132d35e.chunk.js"
+  },
+  {
+    "revision": "28a66632071b0ad6a07bb7c465dc9b43",
+    "url": "/portfolio/static/js/2.0132d35e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86b0b4d7b0984a4cb1ad",
+    "url": "/portfolio/static/js/main.477b29ad.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "13ece6c38f6982c51600c5aef0820138",
+    "url": "/portfolio/static/media/goboat.13ece6c3.jpg"
+  },
+  {
+    "revision": "5ec6cee748a865cdc392781848d90461",
+    "url": "/portfolio/static/media/mindseed.5ec6cee7.png"
+  },
+  {
+    "revision": "d69ed0c6f86974e046512af9505912c0",
+    "url": "/portfolio/static/media/royalbrinkman.d69ed0c6.png"
+  },
+  {
+    "revision": "c7ffe691a56cc1cb02ad757a07d1df07",
+    "url": "/portfolio/static/media/thomaskrief.c7ffe691.png"
+  }
+]);
