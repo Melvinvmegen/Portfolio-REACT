23481f17580d11b5f3747b851b8c6f752a2ac6ad
Melvin <melvin.vmegen@gmail.com>
2020-03-11 17:28:52 +0100

    active menu on scroll

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -7,7 +7,7 @@ const Modal = (props) => (
     <BackDrop show={props.show} clicked={props.modalClosed} />
     <div className={classes.Modal}
       style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        transform: props.show ? 'translateY(0)' : 'translateY(-150vh)',
         opacity: props.show ? "1" : "0"
       }}>
       {props.children}
--- src/Containers/Header/Nav/Nav.js
+++ src/Containers/Header/Nav/Nav.js
@@ -4,27 +4,77 @@ import { Breakpoint } from 'react-socks';
 
 class Nav extends Component {
   static contextType = ContentContext
+  state = {
+    main: [
+      { id: 1, name: 'Developpeur Front-end', active: false },
+      { id: 2, name: 'Back-end', active: false },
+      { id: 3, name: 'Marketeur Digital', active: false },
+      { id: 4, name: 'Mes Projets', active: false },
+    ]
+  }
 
   toggleActive = (index) => {
-    const links = [...this.context.main]
+    const links = [...this.state.main]
     const alreadyActive = links.filter(link => link.active === true);
     if (alreadyActive.length > 0) {
       alreadyActive[0].active = false
       links[index].active = !links[index].active
-      this.context.main = links
+      this.setState({ main: links })
     }
     else {
       links[index].active = !links[index].active
-      this.context.main = links
+      this.setState({ main: links })
     }
   }
 
+  toggleScroll = () => {
+    let ref = window.pageYOffset
+    const links = [...this.state.main]
+    const sectionOne = document.querySelector('#section-one')
+    const sectionTwo = document.querySelector('#section-two')
+    const sectionThree = document.querySelector('#section-three')
+    const sectionGallery = document.querySelector('.gallery-full')
+    const sectionTestimonials = document.querySelector('.section-testimonial')
+    if (ref >= sectionOne.offsetTop && ref <= sectionTwo.offsetTop) {
+      links[0].active = true
+      this.setState({ main: links })
+    }
+    else if (ref >= sectionTwo.offsetTop && ref <= sectionThree.offsetTop) {
+      links[0].active = false
+      links[1].active = true
+      this.setState({ main: links })
+    }
+    else if (ref >= sectionThree.offsetTop && ref <= sectionGallery.offsetTop) {
+      links[1].active = false
+      links[2].active = true
+      this.setState({ main: links })
+    }
+    else if (ref >= sectionGallery.offsetTop && ref <= sectionTestimonials.offsetTop) {
+      links[2].active = false
+      links[3].active = true
+      this.setState({ main: links })
+    }
+    else if (ref >= sectionGallery.offsetTop) {
+      links[3].active = false
+      this.setState({ main: links })
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.toggleScroll.bind(this).bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.toggleScroll.bind(this).bind(this));
+  }
+
   render() {
+
     return (
       <div className="main-header-center" >
         <Breakpoint large up className="nav-menu">
-          {this.context.main.map((link, index) => {
-            return <div className="nav-menu-item" key={link.id} onClick={() => this.toggleActive(index)} className={link.active || window.location.hash == "#" + link.name.replace(/ /g, "-") ? "nav-anim" : ""} >
+          {this.state.main.map((link, index) => {
+            return <div className="nav-menu-item" key={link.id} onClick={() => this.toggleActive(index)} className={link.active || window.location.hash == "#" + link.name.replace(/ /g, "-") ? "nav-anim" : "nav-item"} >
               <a href={"#" + link.name.replace(/ /g, "-")} data-target={link.id}>
                 <span className="text" data-target={link.id}>{link.name}</span>
                 <span className="line -bottom"></span>
@@ -33,7 +83,7 @@ class Nav extends Component {
           })}
         </Breakpoint>
         <Breakpoint medium down className="nav-menu-mobile">
-          {this.context.main.map(link => {
+          {this.state.main.map(link => {
             return <div className="nav-menu-item" key={link.id}>
               <a href={"#" + link.name.replace(/ /g, "-")} className=""><span className="text">{link.name}</span></a>
               <span className="line -bottom"></span>
